7d65b0ef4b95d0d0ff8d50dc2601a6147ea32252
jdsteege <jdsteege@gmail.com>
2022-06-02 13:54:17 -0500

    Geolocation test

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  latLong: string = 'loading...';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.latLong = 'no support';
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latLong = position.coords.longitude.toString();
+        this.latLong += ', ' + position.coords.latitude.toString();
+      });
+    }
   }
-
 }
